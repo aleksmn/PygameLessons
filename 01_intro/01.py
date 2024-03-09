908d49468853639cbf8190f5896c6f67ea7dca40
aleksmn <al-mn@yandex.ru>
2024-03-09 11:19:54 +0300

    fix

--- 01_intro/01.py
+++ 01_intro/01.py
@@ -1,20 +1,30 @@
 import pygame as pg
 import random
 
+
+
+# Создаем окно для игры
 # Размер окна
 size = (500, 500)
+# Окно
 screen = pg.display.set_mode(size)
 
-fps = 4
+# Цвета
+my_colors = ["#ff00ff", "#00ff00", "#0000ff", "#ff0000"]
+
+# Количество кадров секунду
+fps = 1
 
 clock = pg.time.Clock()
 
+# Игровой цикл
 while True:
-    r = random.randint(0, 255)
-    g = random.randint(0, 255)
-    b = random.randint(0, 255)
-    screen.fill(pg.Color(r, g, b))
-    
-    pg.display.flip()
 
+    rand_color = random.choice(my_colors)
+    
+    screen.fill(rand_color)
+
+    # Обновляем экран
+    pg.display.flip()
+    # Переходим к следующем кадру
     clock.tick(fps)
