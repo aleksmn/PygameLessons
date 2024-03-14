bf0bcb9364e715117d30d82f4db93b42d1649169
aleksmn <al-mn@yandex.ru>
2024-03-14 19:43:50 +0300

    fix

--- 01_intro/03_1.py
+++ 01_intro/03_1.py
@@ -3,50 +3,61 @@ import pygame as pg
 size = (800, 800)
 screen = pg.display.set_mode(size)
 # Заголовок окна
-pg.display.set_caption('Координаты')
+pg.display.set_caption('Движение')
 
+# Создадим прямоугольник
 small_rect = pg.Rect(0, 0, 60, 30)
 
-# small_rect.left = 100
-# small_rect.top = 200
-
+# изменяем размеры прямоугольника
 small_rect.size = (100, 100)
 
-small_rect.center = size[0]/2, size[1]/2
+# изменяем расположение прямоугольника
+small_rect.center = (size[0]//2, size[1]//2)
 
-direction = 'right'
 
+direction = 'left'
+
+# Задаем FPS
 fps = 60
 clock = pg.time.Clock()
 
+
+# Игровой цикл
 running = True
 while running:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
+    #--------------------------- 
 
-    # Увеличение прямоугольника
+    # Изменяем размер
     # small_rect.width += 1
     # small_rect.height += 1
     # small_rect.center = size[0]/2, size[1]/2
+            
+    # Движение
 
-
-    # Перемещение прямоугольника
-
+    if small_rect.right == size[0]:      
+        direction = "left"
     if small_rect.left == 0:
         direction = "right"
-    if small_rect.right == size[0]:
-        direction = "left"
-
 
     if direction == "right":
         small_rect.x += 1
     if direction == "left":
         small_rect.x -= 1
+     
 
-    screen.fill(pg.Color("cyan"))
-    pg.draw.rect(screen, pg.Color("orange"), small_rect)
+
+
+    # Заливка фона
+    screen.fill("cyan")
     
+    # Отрисовка прямоугольника
+    pg.draw.rect(screen, "orange", small_rect)
+
+
+    # Переключаем кадр
     pg.display.flip()
     clock.tick(fps)
 
