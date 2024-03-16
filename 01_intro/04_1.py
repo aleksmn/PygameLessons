3933ca7c4ba13e9da2f0c1919b5ee76f4c967648
aleksmn <al-mn@yandex.ru>
2024-03-16 11:48:21 +0300

    fix character

--- 01_intro/04_1.py
+++ 01_intro/04_1.py
@@ -6,27 +6,34 @@ screen = pg.display.set_mode(size)
 # Заголовок окна
 pg.display.set_caption('Изображения и звук')
 
-# Добавляем иконку
-icon = pg.image.load('icon.png')
-pg.display.set_icon(icon)
 
+# Персонаж
+character = pg.image.load("Character.png")
+# изменяем размер картинки
+character = pg.transform.scale(character, (250, 420))
+# задаем расположение
+character_rect = character.get_rect()
+character_rect.center = (size[0] // 2, size[1] // 2)
+
+
+# FPS
 fps = 100
 clock = pg.time.Clock()
-
-character = pg.image.load("Character.png")
-character = pg.transform.scale(character, (300, 477))
 
 running = True
 while running:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
+    
+    # Заливка фона
+    screen.fill("orange")
 
-    screen.fill(pg.Color("cyan"))        
 
+    # Размещаем картинку на экране
+    screen.blit(character, character_rect)
 
-
-
+      
     pg.display.flip()
     clock.tick(fps)
 
