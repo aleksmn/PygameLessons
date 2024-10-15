a82b9f8277309fb58cfed21f489058b1518d734d
aleksmn <al-mn@yandex.ru>
2024-10-15 20:58:05 +0300

    fix

--- 03_dragon/main copy.py
+++ 03_dragon/main copy.py
@@ -8,26 +8,37 @@ class Dragon(pg.sprite.Sprite):
         pg.sprite.Sprite.__init__(self)
 
         self.image = pg.image.load("images/dragon.png")
+        # изменяем размер
         self.image = pg.transform.scale(self.image, (80, 80))
-
-        # self.image = pg.transform.flip(self.image, True, False)
-
+        # получим прямоугольник от кортинки
         self.rect = self.image.get_rect()
+        # указываем расположение
         self.rect.topleft = (25, 25)
 
         self.direction = 'right'
 
-
     def update(self):
-        # Get a list of all keys pressed down
+        # Получаем список нажатых клавиш
         keys = pg.key.get_pressed()
 
-        if keys[pg.K_LEFT]:
-            self.rect.x -= VELOCITY
+        if keys[pg.K_RIGHT] and self.rect.right < WINDOW_WIDTH:
+            self.rect.x += 5
 
-        if keys[pg.K_RIGHT]:
-            self.rect.x += VELOCITY
+        if keys[pg.K_LEFT] and self.rect.left > 0:
+            self.rect.x -= 5
 
+
+
+
+
+
+class Coin(pg.sprite.Sprite):
+    def __init__(self):
+        pg.sprite.Sprite.__init__(self)
+
+        self.image = pg.image.load("images/coin.png")
+        self.rect = self.image.get_rect()
+        self.rect.center = (WINDOW_WIDTH//2, WINDOW_HIGHT//2)
 
 
 
@@ -36,10 +47,11 @@ pg.init()
 
 # Создаем игровой дисплей
 WINDOW_WIDTH = 600
-WINDOW_HIGHT = 300
+WINDOW_HIGHT = 600
 screen = pg.display.set_mode((WINDOW_WIDTH, WINDOW_HIGHT))
 pg.display.set_caption('Hungry Dragon!')
 
+VELOCITY = 5
 
 # Устнавливаем FPS
 FPS = 60
@@ -50,16 +62,15 @@ BLACK = (0, 0, 0)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
 
-# Скорость движения
-VELOCITY = 5
+
 # Очки
 score = 0
 
+
+
 # Создаем объекты
 dragon = Dragon()
-
-
-
+coin = Coin()
 
 # Игровой цикл
 running = True
@@ -69,14 +80,26 @@ while running:
             running = False
 
 
-    dragon.update()       
-
     # Заливка экрана
-    screen.fill((BLACK))
+    screen.fill(BLACK)
 
+    dragon.update()
 
     # Отрисовка спрайтов
     screen.blit(dragon.image, dragon.rect)
+    screen.blit(coin.image, coin.rect)
+
+
+    # Проверяем коллизии двух спрайтов
+    if dragon.rect.colliderect(coin.rect):
+        score += 1
+        print(score)
+        # Добавим звук
+        ...
+        # Переместим монетку в случайное место
+        coin.rect.x = random.randint(0, WINDOW_WIDTH - coin.rect.width)
+        coin.rect.y = ...
+
 
 
 
